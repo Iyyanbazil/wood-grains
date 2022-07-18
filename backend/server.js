3b81b9c522e602cc88d8bc40b879094c4cd7a382
ayyan <ayyanbazil000@gmail.com>
2022-07-18 08:08:57 -0700

    make it batter

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,20 @@ const app=express()
 let cors = require('cors');
 // let bodyParser = require('body-parser');
 app.use(cors());
-
+require("dotenv").config();
 // app.use(express.bodyParser({limit: '50mb'}));
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
-const Port=8000;
+const Port=process.env.PORT;
 const connectDb=require("./database/connection")
 const Proute=require("./database/route")
+// app.search("port",Port)
 // app.get("/",(req,res)=>{
 //      res.send({msg:"iam base route did it"})
 // })
 app.use("/",Proute)
-app.listen(Port,()=>{
+app.listen(process.env.PORT || 8000,()=>{
     console.log(`Server is running on Port ${Port}`)
 })
 connectDb()
